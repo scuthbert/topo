83c97568e7d6c95833e34648973e855dee5045eb
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2019-09-02 15:31:13 -0700

    Adding updated script

--- parseInto2D.py
+++ parseInto2D.py
@@ -1,27 +1,29 @@
 import numpy as np
 from PIL import Image
+from itertools import chain
 
 # CHANGE ME
+top_row = 550
+bottom_row = 4500
 
-
+slices = range(1000, 2000, 500) #chain(range(-900, -200, 100), range(-200, 100, 20), range(100, 300, 100), range(300, 1000, 150), range(1000, 1500, 500))
 
 # DON'T CHANGE ME
-data = np.loadtxt(open("./psdem.csv", "rb"), skiprows=550, max_rows=3950, delimiter=",", dtype=np.int16)
+raw_data = np.loadtxt(open("./psdem.csv", "rb"), skiprows=top_row, max_rows=bottom_row-top_row, delimiter=",", dtype=np.int16)
 
-# Gen a whole pile of things
-data = np.interp(data, (5, 55), (0, 255))
+previous = -1000
+for i in slices:
+    data = np.interp(raw_data, (previous, i), (0, 255))
 
-img = Image.fromarray( data )       # Create a PIL image
-img = img.convert("RGBA")
-pixdata = img.load()
+    img = Image.fromarray( data )       # Create a PIL image
+    img = img.convert("RGBA")
+    pixdata = img.load()
 
-width, height = img.size
-for y in range(height):
-    for x in range(width):
-        if pixdata[x, y] == (0, 0, 0, 255):
-            pixdata[x, y] = (255, 255, 255, 0)
+    width, height = img.size
+    for y in range(height):
+        for x in range(width):
+            if pixdata[x, y] == (0, 0, 0, 255):
+                pixdata[x, y] = (255, 255, 255, 0)
 
-img.save("psdem.png", "PNG")
-
-# Create a 1024x1024x3 array of 8 bit unsigned integers
-#data = np.loadtxt(open("./psdem.asc", "rb"), delimiter=" ", dtype=np.int16, usecols=range(12500, 16006))
+    img.save("./export/" + str(previous) + "to" + str(i) + ".png", "PNG")
+    previous = i
