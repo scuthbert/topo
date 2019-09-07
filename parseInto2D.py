4344e422babadcb8c2b2d33a472121cfdaeec54e
Samuel Cuthbertson <samcuthb@amazon.com>
2019-09-06 17:13:44 -0700

    Threadting

--- parseInto2D.py
+++ parseInto2D.py
@@ -1,21 +1,15 @@
 import numpy as np
 from PIL import Image
 from itertools import chain
+from subprocess import run
+from multiprocessing import Process
 
-# CHANGE ME
-top_row = 550
-bottom_row = 4500
 
-slices = chain(range(-900, -200, 100), range(-200, 100, 20), range(100, 300, 100), range(300, 1000, 150), range(1000, 2000, 500))
 
 # DON'T CHANGE ME
-raw_data = np.loadtxt(open("./psdem.csv", "rb"), skiprows=top_row, max_rows=bottom_row-top_row, delimiter=",", dtype=np.int16)
-
-previous = -1000
-for i in slices:
-    data = np.interp(raw_data, (previous, i), (0, 255))
-
-    img = Image.fromarray( data )       # Create a PIL image
+def gen_for_slice(bottom, top):
+    data = np.interp(raw_data, (bottom, top), (0, 255))
+    img = Image.fromarray( data )
     img = img.convert("RGBA")
     pixdata = img.load()
 
@@ -23,7 +17,38 @@ for i in slices:
     for y in range(height):
         for x in range(width):
             if pixdata[x, y] == (0, 0, 0, 255):
-                pixdata[x, y] = (255, 255, 255, 0)
+                pixdata[x, y] = (0, 0, 0, 0)
 
-    img.save("./export/" + str(previous) + "to" + str(i) + ".png", "PNG")
-    previous = i
+    filename = "./export/" + str(bottom) + ".png"
+    filenameBW = "./export/BW" + str(bottom) + ".bmp"
+
+    img.save(filename, "PNG")
+
+    for y in range(height):
+        for x in range(width):
+            if pixdata[x, y] != (0, 0, 0, 0):
+                pixdata[x, y] = (255, 255, 255, 255)
+
+    img.save(filenameBW, "BMP")
+
+    cmd = "potrace " + filenameBW + " -s -t 100"
+    run(cmd, shell=True, check=True)
+
+    #Change it so that the svg line is hairline black on white w/sed
+    #Call it good?
+
+
+if __name__ == '__main__':
+    # CHANGE ME
+    top_row = 550
+    bottom_row = 4500
+
+    slices = chain(range(-900, -200, 100), range(-200, 100, 20), range(100, 300, 100), range(300, 1000, 150), range(1000, 2000, 500))
+    begin = -1000
+
+    raw_data = np.loadtxt(open("./psdem.csv", "rb"), skiprows=top_row, max_rows=bottom_row-top_row, delimiter=",", dtype=np.int16)
+
+    for i in slices:
+        p = Process(target=gen_for_slice, args=(begin,i,))
+        p.start()
+        begin = i
